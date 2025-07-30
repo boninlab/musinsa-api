761ea3a94c57c77c6f94408a2b463b22f05e746d
jhWon <boan1st@gmail.com>
2025-07-30 21:16:22 +0900

    init

---
+++ src/main/kotlin/com/musinsa/MusinsaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.musinsa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusinsaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MusinsaApiApplication>(*args)
+}
